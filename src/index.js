b71f1f5d5c2ab9ed51e48d70d2b9cf2b4843d188
Frezeh <ezehfrank87@gmail.com>
2021-09-13 08:21:41 +0100

    Edit 2

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   useQuery,
+//   gql
+// } from "@apollo/client";
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/ConfigureStore'; 
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const store = ConfigureStore();
+// const store = ConfigureStore();
 
 // const client = new ApolloClient({
 //   // uri: 'https://71z1g.sse.codesandbox.io/',
