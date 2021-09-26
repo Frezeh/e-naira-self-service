39a31c21af013d64c057a6c9fb8a5575c3fcd84b
Frezeh <ezehfrank87@gmail.com>
2021-09-26 20:48:53 +0100

    baseurl edit

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -1,2 +1,2 @@
 // export const baseUrl = 'http://localhost:3000/';
-export const baseUrl = "https://enairaselfservice.herokuapp.com/";
+export const baseUrl = "https://enairaselfservie.herokuapp.com/";
